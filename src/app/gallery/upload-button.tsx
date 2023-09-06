1227e0ed3d907e48793a8adb7fb96a54d5553ba4
ABDULLAH BIRJEES <115080318+AbdullahBirjees@users.noreply.github.com>
2023-09-06 17:48:36 +0500

    first commit

---
+++ src/app/gallery/upload-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CldUploadButton } from "next-cloudinary";
+import { UploadResult } from "../page";
+import { useRouter } from "next/navigation";
+
+export default function UploadButton() {
+  const router = useRouter();
+  return (
+    <Button asChild>
+      <CldUploadButton
+        onUpload={(result: UploadResult | any) => {
+          //   setImageId(result.info.public_id);
+          setTimeout(() => {
+            router.refresh();
+          }, 1000);
+        }}
+        uploadPreset="uojdzhsl"
+      >
+        <div className="flex gap-2 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+        </div>
+      </CldUploadButton>
+    </Button>
+  );
+}
---
+++ src/components/add-to-album-dialog.tsx
@@ -0,0 +1,58 @@
+import { SearchResult } from "@/app/gallery/page";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FolderPlus } from "lucide-react";
+import {useState} from 'react'
+import { addImageToAlbum } from "./action";
+
+export function AddToAlbumDialog({image}:{image: SearchResult}) {
+
+  const [albumName, setAlbumName] = useState("");
+  const [open, setOpen] = useState(false)
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button variant="ghost">
+          <FolderPlus className="mr-2 h-8 w-8" />
+          <span>Add to Album</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add to Album</DialogTitle>
+          <DialogDescription>
+            Type na album you want to move this image into
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Album
+            </Label>
+            <Input 
+            onChange={(e)=> setAlbumName(e.currentTarget.value)}
+            id="album-name" value={albumName} className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button 
+          onClick={async()=>{
+            setOpen(false);
+            await addImageToAlbum(image, albumName)
+          }}
+          type="submit">Add to Album</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}

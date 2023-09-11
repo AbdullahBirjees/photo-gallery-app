627609f0bbb60d0adaff9990f2f1236f137010de
ABDULLAH BIRJEES <115080318+AbdullahBirjees@users.noreply.github.com>
2023-09-11 21:35:24 +0500

    more change

--- src/app/albums/album-card.tsx
+++ src/app/albums/album-card.tsx
@@ -9,8 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Folder } from "./page";
 import Link from "next/link";
 

209a9831bbe746b848273e0ccf724c0524c583c3
ABDULLAH BIRJEES <115080318+AbdullahBirjees@users.noreply.github.com>
2023-09-11 19:12:17 +0500

    final changes

---
+++ src/app/favorites/favorites-list.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ImageGrid } from "@/components/image-grid";
+import { CloudinaryImage } from "../../components/cloudinary-image";
+import { SearchResult } from "../gallery/page";
+import { useState, useEffect } from "react";
+
+export default function FavoritesList({
+  initialResources,
+}: {
+  initialResources: SearchResult[];
+}) {
+  const [resources, setResources] = useState(initialResources);
+  useEffect(() => {
+    setResources(initialResources);
+  }, [initialResources]);
+  return (
+    <ImageGrid images={resources}
+    getImage={(imagedata: SearchResult) => {
+      return (
+        <CloudinaryImage
+          key={imagedata.public_id}
+          imagedata={imagedata}
+          width="400"
+          height="300"
+          alt="an image of something"
+          onUnheart={(unheartedResource) => {
+            setResources((currentResources) =>
+              currentResources.filter(
+                (resource) => resource.public_id !== unheartedResource.public_id
+              )
+            );
+          }}
+        />
+      );
+    }}/>
+
+  );
+}
